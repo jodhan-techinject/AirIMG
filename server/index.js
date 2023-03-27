4aa3013abd56bd940c8f346e34a5a0bd2d35b6bc
jodhan <jodhan.saji@gmail.com>
2023-03-28 01:19:00 +0530

    backend updated

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use('/api/v1/post',postRoutes)
