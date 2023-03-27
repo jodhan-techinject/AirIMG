e8cdb8b678ef3bcd318191cd54ebbbaacf77e8b6
jodhan <jodhan.saji@gmail.com>
2023-03-28 01:04:43 +0530

    CORS updated

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js'
 dotenv.config();
 
 const app = express();
-app.use(cors({"orgin":"*"}));
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 
